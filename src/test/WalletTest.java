05588db8ad10454ae1c650994fb143ade2595f7b
Karol <karol_skupien@epam.com>
2016-06-23 16:34:09 +0200

    Second chapter - degenerated objects. In this commit we eliminated side effects which appeared after previous chapter. There was a mistake that our object doesn't change after times method was invoked. We repaired this here.

--- src/test/WalletTest.java
+++ src/test/WalletTest.java
@@ -6,8 +6,10 @@ public class WalletTest {
     @Test()
     public void multiplicationTest() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        Assert.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assert.assertEquals(15, product.amount);
     }
 
 }
