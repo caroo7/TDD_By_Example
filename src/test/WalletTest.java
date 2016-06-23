0c08c445b8543ade8463bd6a903c060665d74c31
Karol <karol_skupien@epam.com>
2016-06-23 17:04:08 +0200

    Chapter 5 - Franks, dollars.. Implementation of different currency as the same class as before

--- src/test/WalletTest.java
+++ src/test/WalletTest.java
@@ -11,6 +11,13 @@ public class WalletTest {
     }
 
     @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        Assert.assertEquals(new Franc(10), five.times(2));
+        Assert.assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality() {
         Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
         Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
