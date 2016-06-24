9194a81b7a3aeaa9afb3d20d4b77ff83bcc312eb
Karol <karol_skupien@epam.com>
2016-06-24 11:25:43 +0200

    Chapter 8 - creating objects - in this chapter we crated static methods in abstract class which are producents for our specific money implementations.

--- src/main/Money.java
+++ src/main/Money.java
@@ -1,12 +1,22 @@
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount
                 && getClass().equals(o.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 
 }
